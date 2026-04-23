b4e2f79f65f5c93f1ab9f15ff08c71ce1526bdbf
jararasifdev <jarar.asif@devsinc.com>
2026-04-23 21:19:59 +0500

    added pydantic model for partial update of tasks,and models for notes and tag

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
 from datetime import date
 
+from typing import Optional
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[str] = None
+    priority: Optional[str] = None
+    due_date: Optional[date] = None
+    
 class TaskCreate(BaseModel):
     title: str
     description: str
