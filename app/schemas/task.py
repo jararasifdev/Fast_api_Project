89083079af8344331e65a8f0dc92818fccb3e1f3
jararasifdev <jarar.asif@devsinc.com>
2026-04-23 22:07:34 +0500

    added model for returing paginated reponse and also added some extra field in task response

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from datetime import date
 
-from typing import Optional
+from typing import Optional,List
 
 class TaskUpdate(BaseModel):
     title: Optional[str] = None
@@ -9,19 +9,29 @@ class TaskUpdate(BaseModel):
     status: Optional[str] = None
     priority: Optional[str] = None
     due_date: Optional[date] = None
-    
+
 class TaskCreate(BaseModel):
     title: str
     description: str
     status: str
     priority: str
     due_date: date
-    created_at:date
 
 class TaskOut(BaseModel):
     id: int
     title: str
+    description: str
     status: str
+    priority: str
+    due_date: date
+    created_at: date
 
     class Config:
         from_attributes = True
+
+class PaginatedTasks(BaseModel):
+    page: int
+    limit: int
+    total_tasks: int
+    total_pages: int
+    data: List[TaskOut]        
