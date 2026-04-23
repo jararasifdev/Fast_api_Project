7e3c4c201472994b17c4684b4daa3780c97c4775
jararasifdev <jarar.asif@devsinc.com>
2026-04-23 19:20:04 +0500

    Added configuration for hashing of password and creation of jwt token

---
+++ app/core/security.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+pwd_context = CryptContext(schemes=["bcrypt"])
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=60)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
